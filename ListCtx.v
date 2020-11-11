c2bd10da664e5184a859796b5e6b0a51e2867bdc
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2020-11-12 00:59:25 +0100

    Repair ListCtx to use later

--- ListCtx.v
+++ ListCtx.v
@@ -4,6 +4,7 @@ Require Import BaseProofs.
 Require Import Coq.Bool.Bool.
 Require Import Coq.Logic.Classical_Prop.
 Require Import Coq.Logic.Classical_Pred_Type.
+Require Import Coq.Logic.Decidable.
 
 Module Type KeyModuleType.
 
@@ -127,7 +128,7 @@ Module Type ListCtx
   Inductive contains : T -> K -> V -> Prop :=
   | contains_append : forall s s' k v, s = append s' k v -> contains s k v
   | contains_append_set : forall s' k v k' v',
-    contains s' k' v' -> contains (append s' k v) k' v'.
+    contains s' k' v' -> k <> k' -> contains (append s' k v) k' v'.
 
   Example unittest_contains_overwrite : forall s k k' v v' v'',
     k <> k' ->
@@ -146,15 +147,45 @@ Module Type ListCtx
     try apply contains_append. apply contains_append_set. 
   Abort.
 
+  (* Contains Key *)
+  Inductive contains_key : T -> K -> Prop :=
+    contains_key_contains : forall (A : T) (k : K) (v : V),
+      contains A k v -> contains_key A k.
+
+  Proposition contains_key_extend : forall (A : T) (k k' : K) (v' : V),
+    contains_key A k -> contains_key (append A k' v') k.
+  Proof.
+    intros. inversion H.
+    assert (Hkk' : k = k' \/ k <> k'). { apply classic. } inversion Hkk'.
+    - assert (Hkv' : contains (append A k' v') k v').
+      { apply contains_append with (s' := A). rewrite <- H3. reflexivity. }
+      apply contains_key_contains with (v := v'). apply Hkv'.
+    - assert (Hkv : contains (append A k' v') k v).
+      { apply contains_append_set; auto. }
+      apply contains_key_contains with (v := v). apply Hkv.
+  Qed.
+
+  Corollary not_contains_key_shrink : forall (A : T) (k k' : K) (v' : V),
+    ~ contains_key (append A k' v') k -> ~ contains_key A k.
+  Proof.
+    intros. unfold not in H. unfold not. intros Hfalse.
+    apply contains_key_extend with (k' := k') (v' := v') in Hfalse. apply H in Hfalse.
+    inversion Hfalse.
+  Qed.
+
   (* Union *)
   Lemma union_l : forall (B C : T) (k : K) (v : V), 
-    contains B k v -> contains (B o C) k v.
+    contains B k v -> ~ contains_key C k -> contains (B o C) k v.
   Proof.
     intros. induction C.
     - rewrite -> id_r. apply H.
     - assert (HX : B o append C k0 v0 = append (B o C) k0 v0).
       { apply append_concat. reflexivity. } rewrite -> HX.
-      apply contains_append_set. apply IHC.
+      apply contains_append_set.
+      + apply not_contains_key_shrink in H0. apply IHC in H0. auto.
+      + unfold not. unfold not in H0. revert H0. rewrite -> contrapositive.
+        * intros Hkk. rewrite -> Hkk. eapply contains_key_contains. eapply contains_append. reflexivity.
+        * unfold decidable. apply classic.
   Qed.
 
   Lemma union_r : forall (B C : T) (k : K) (v : V),
@@ -167,7 +198,7 @@ Module Type ListCtx
       eapply contains_append. reflexivity.
     - assert (HX : B o append s' k v = append (B o s') k v).
       { apply append_concat. reflexivity. } rewrite -> HX.
-      apply contains_append_set. apply IHcontains. 
+      apply contains_append_set. apply IHcontains. auto.
   Qed.
 
   Lemma reunion : forall (B C : T) (k : K) (v : V),
@@ -182,7 +213,7 @@ Module Type ListCtx
         right. eapply contains_append. reflexivity.
       + subst s' k1 v1 k' v'. apply IHC in H5. inversion H5.
         * left. apply H0.
-        * right. apply contains_append_set. apply H0.
+        * right. apply contains_append_set. apply H0. auto.
   Qed.
 
   (* Equivalence *)
@@ -228,16 +259,74 @@ Module Type ListCtx
     - apply H2.
   Qed.
 
-  (* Commutativity *)
-  Lemma eq_commut : forall (B C : T), B o C ≡ C o B.
+  (* Duplicate Keys *)
+  Inductive duplicated : T -> Prop :=
+  | duplicated_append : forall (A : T) (k : K) (v' : V),
+      contains_key A k -> duplicated (append A k v')
+  | duplicated_extend : forall (A : T) (k : K) (v' : V),
+      duplicated A -> duplicated (append A k v').
+
+  Definition unique_paired (A : T) : Prop := ~ duplicated A.
+
+  Proposition unique_paired_shrink : forall (A : T) (k : K) (v : V),
+    unique_paired (append A k v) -> unique_paired A.
   Proof.
-    intros B C. unfold eq. split.
+    intros. unfold unique_paired. unfold not. unfold unique_paired in H. unfold not in H. revert H.
+    apply contrapositive. { unfold decidable. apply classic. } apply duplicated_extend.
+  Qed.
+
+  Lemma unique_paired_either_or : forall (A B : T) (k : K),
+    unique_paired (A o B) -> contains_key A k -> ~ contains_key B k.
+  Proof.
+    intros A B k Hu HcAk. unfold not. intros HcBk. induction B.
+    - inversion HcBk. inversion H. inversion H2.
+    - assert (Hu' : unique_paired (A o append B k0 v)). { auto. }
+      rewrite -> append_concat with (s' := B) (k := k0) (v := v) in Hu; auto.
+      apply unique_paired_shrink in Hu. apply IHB in Hu; auto. inversion HcBk. inversion H.
+  Admitted.
+
+  Lemma unique_paired_or_either : forall (A B : T) (k : K),
+    unique_paired (A o B) -> contains_key B k -> ~ contains_key A k.
+  Admitted.
+
+  (* Well-Definedness and Contains Uniqueness *)
+  Inductive ill_defined : T -> Prop :=
+    duplicate_values : forall (A : T) (k : K) (v v' : V),
+      contains A k v -> contains A k v' -> v <> v' -> ill_defined A.
+
+  Definition well_defined (A : T) : Prop := ~ ill_defined A.
+
+  Lemma contains_unique : forall (A : T) (k : K),
+    well_defined A -> contains_key A k -> (exists! (v : V), contains A k v).
+  Proof.
+    intros A k H H'. inversion H'. subst A0 k0. exists v. unfold unique. split.
+    - apply H0.
+    - intros v'. unfold well_defined in H. unfold not in H. intros.
+      (* reductio ad absurdum *)
+      assert (Hx : v = v' \/ v <> v'). { apply classic. }
+      inversion Hx; try apply H2. assert (Hf : ill_defined A). 
+      { eapply duplicate_values. apply H0. apply H1. apply H2. }
+      apply H in Hf. inversion Hf.
+  Qed.
+
+  Fixpoint to_function (A : T) (none : V) (k : K) : V :=
+    match A with
+    | append B k' v' => if KM.equal k k' then v' else to_function B none k
+    | empty => none
+    end.
+
+  (* Commutativity *)
+  Lemma eq_commut : forall (B C : T), unique_paired (B o C) -> B o C ≡ C o B.
+  Proof.
+    intros B C Hwd. unfold eq. split.
     - unfold subseteq. intros. apply reunion in H. inversion H.
       + apply union_r. apply H0.
-      + apply union_l. apply H0.
+      + apply union_l. apply H0. eapply unique_paired_or_either. apply Hwd.
+        eapply contains_key_contains. apply H0.
     - unfold subseteq. intros. apply reunion in H. inversion H.
       + apply union_r. apply H0.
-      + apply union_l. apply H0.
+      + apply union_l. apply H0. eapply unique_paired_either_or. apply Hwd.
+        eapply contains_key_contains. apply H0.
   Qed.
 
   (* Axioms of Extionsionality *)
@@ -261,42 +350,30 @@ Module Type ListCtx
   Qed.
 
   (* Commutativity *)
-  Lemma commut : forall (B C : T), B o C = C o B.
+  Lemma commut : forall (B C : T), unique_paired (B o C) -> B o C = C o B.
   Proof.
-    intros. apply extensionality. apply eq_commut.
+    intros. apply extensionality. apply eq_commut. auto.
   Qed.
 
   (* Exchange *)
-  Lemma exchange : forall A B C D, A o B o C o D = A o C o B o D.
+  Lemma exchange : forall A B C D, unique_paired (B o C) -> A o B o C o D = A o C o B o D.
   Proof.
-    intros.
-    assert (H : B o C = C o B). { apply commut. }
+    intros A B C D Hu.
+    assert (H : B o C = C o B). { apply commut. auto. }
     assert (H' : B o (C o D) = C o (B o D)). 
       { repeat rewrite <- assoc. rewrite -> H. reflexivity. }
     repeat rewrite -> assoc. rewrite -> H'. reflexivity.
   Qed.
 
   Proposition append_commut : forall s k v k' v',
+      unique_paired (append empty k v o append empty k' v') ->
       append (append s k v) k' v' = append (append s k' v') k v.
   Proof.
     intros. rewrite -> append_to_concat. rewrite -> append_to_concat.
     rewrite <- id_r with (m := s o (append empty k v) o (append empty k' v')).
-    rewrite -> exchange. rewrite -> id_r. repeat rewrite <- append_to_concat.
+    rewrite -> exchange; auto. rewrite -> id_r. repeat rewrite <- append_to_concat.
     reflexivity.
   Qed.
-
-  Lemma contains_exists : forall (S S' : T) (k : K) (v : V),
-    contains S k v -> exists S', S = append S' k v.
-  Proof.
-    intros. induction H.
-    - exists s'. subst. reflexivity.
-    - inversion IHcontains as [s'']. subst s'. exists (append s'' k v). apply append_commut.
-  Qed.
-
-  (* Contains Key *)
-  Inductive contains_key : T -> K -> Prop :=
-    contains_key_contains : forall (A : T) (k : K) (v : V),
-      contains A k v -> contains_key A k.
 
   (* Remove Key *)
   Fixpoint remove (A : T) (k : K) : T :=
@@ -343,7 +420,8 @@ Module Type ListCtx
         assert (HX : ~ contains_key A k').
         { unfold not. intros Hf. inversion Hf. subst A0 k0.
           assert (HX' : contains (append A k v) k' v0). 
-          { apply contains_append_set. apply H1. }
+          { apply contains_append_set. apply H1. unfold not. intros Hfalse. rewrite <- Hfalse in H0.
+            rewrite -> KM.eq_refl in H0. inversion H0. }
           apply contains_key_contains in HX'. apply H in HX'. apply HX'. }
         apply IHA in HX. rewrite -> HX. reflexivity.
   Qed.
@@ -369,9 +447,7 @@ Module Type ListCtx
   Proof.
     intros A. induction A. 
     - intros. simpl. auto.
-    - intros. unfold not in H. (*
-      assert (Ht : (KM.equal k' k && VM.equal v' v) = true \/ KM.equal k' k <> true). 
-      { apply classic. } *) admit.
+    - intros. unfold not in H.
   Admitted.
 
   (* Set Minus *)
@@ -395,37 +471,11 @@ Module Type ListCtx
     intros. simpl. rewrite -> remove_not_contained_pair; auto.
   Qed.
 
-  (* Duplicate Keys *)
-  Inductive duplicated : T -> Prop :=
-    duplicate_keys : forall (A : T) (k : K) (v v' : V),
-      contains A k v -> contains A k v' -> v <> v' -> duplicated A.
-
-  Definition well_defined (A : T) : Prop := ~ duplicated A.
-
-  Lemma contains_unique : forall (A : T) (k : K),
-    well_defined A -> contains_key A k -> (exists! (v : V), contains A k v).
-  Proof.
-    intros A k H H'. inversion H'. subst A0 k0. exists v. unfold unique. split.
-    - apply H0.
-    - intros v'. unfold well_defined in H. unfold not in H. intros.
-      (* reductio ad absurdum *)
-      assert (Hx : v = v' \/ v <> v'). { apply classic. }
-      inversion Hx; try apply H2. assert (Hf : duplicated A). 
-      { eapply duplicate_keys. apply H0. apply H1. apply H2. }
-      apply H in Hf. inversion Hf.
-  Qed.
-
-  Fixpoint to_function (A : T) (none : V) (k : K) : V :=
-    match A with
-    | append B k' v' => if KM.equal k k' then v' else to_function B none k
-    | empty => none
-    end.
-
   (* Pair Uniqueness *)
   Lemma unique_append : forall (A : T) (k : K) (v : V), 
     contains A k v -> append A k v = A.
   Proof.
-    intros. apply extensionality. unfold eq. split; unfold subseteq; intros k' v'.
+   intros. apply extensionality. unfold eq. split; unfold subseteq; intros k' v'.
     - intros H'. inversion H'.
       + subst k0 v0. inversion H0. subst s' k' v'. apply H.
       + subst k0 v0 s' k'0 v'0. apply H5.
